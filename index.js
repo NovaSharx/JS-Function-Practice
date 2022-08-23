997ec47b9a502e68aeb1b222b3c958c7d702c154
Davin <davin-dj@hotmail.com>
2022-08-22 21:29:29 -0400

    Completed Part 3 of the activity.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your Code Here
+
+function start() {
+    let firstAnswer = window.prompt(`Do you head left, or right?`)
+
+    if (firstAnswer === 'left') {
+        goLeft()
+    }
+    else if (firstAnswer === 'right') {
+        goRight()
+    }
+}
+
+function goLeft() {
+    let secondAnswer = window.prompt(`You come across a stray cat. It scampers off down a small hole, just large enough for you to crawl through. Do you follow it, or continue on your path?`)
+    if (secondAnswer === 'back') {
+        start()
+    }
+}
+
+function goRight() {
+    let secondAnswer = window.prompt(`You came across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon all together. Which do you take?`)
+    if (secondAnswer === 'back') {
+        start()
+    }
+}
+
+start()
